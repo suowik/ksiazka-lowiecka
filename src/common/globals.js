52a72e9a2560e918a6d8a8284b2da4a297a7fb35
Paweł Słowik <pawel.slowik@schibsted.pl>
2017-07-21 15:51:33 +0200

    production env

--- src/common/globals.js
+++ src/common/globals.js
@@ -1,4 +1,4 @@
-const API_URL = "http://hunting-book-api.herokuapp.com";
+const API_URL = "https://hunting-book-api.herokuapp.com";
 //const API_URL = "http://localhost:3001";
 export default {
     API_URL: API_URL
