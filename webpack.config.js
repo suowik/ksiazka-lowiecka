4a5f32fddd8010f68c35f89c5f96107307f55301
Paweł Słowik <pawel.slowik@schibsted.pl>
2017-07-31 20:57:22 +0200

    process env

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     plugins: [
         new webpack.DefinePlugin({
             'process.env': {
-                'API_URL': JSON.stringify(process.env.ROOT_URL)
+                'API_URL': JSON.stringify(process.env.API_URL)
             }
         })
     ],
